3a92e938affded9e5c27e1e5c65532e11d20082e
Kyle Feng <kylezete@gmail.com>
2017-11-01 14:35:42 +0000

    add login function

---
+++ AccountBook/AccountBook/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountBook.Models
+{
+    class User
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+
+        public User() { }
+
+        public User(string usetname, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
+    }
+}
--- AccountBook/AccountBook/Views/login.xaml.cs
+++ AccountBook/AccountBook/Views/login.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using AccountBook.Models;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -26,6 +29,65 @@ namespace AccountBook.Views
         {
             this.InitializeComponent();
         }
-        
+
+        private void tbToReg_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(register));
+        }
+
+        private async void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            //lock the button, avoid multi requests at the same time
+            this.btnLogin.IsEnabled = false;
+
+            //Create an HTTP client object
+            Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
+
+            //Get User
+            User user = new User();
+            user.username = txtUsername.Text;
+            user.password = txtPassword.Password;
+
+            //Set uri
+            Uri requestUri = new Uri("http://54.246.215.224:8080/login?username="+user.username+"&password="+user.password);
+            
+            //Send the GET request asynchronously and retrieve the response as a string.
+            Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
+            string httpResponseBody = "";
+
+            try
+            {
+                //Send the GET request
+                httpResponse = await httpClient.GetAsync(requestUri);
+                httpResponse.EnsureSuccessStatusCode();
+                httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message);
+            }
+
+            string msg;
+            if (httpResponseBody == "logged")
+            {
+                msg = $"login success.";
+                await new MessageDialog(msg).ShowAsync();
+                this.Frame.Navigate(typeof(MainPage));
+            }
+            else
+            {
+                msg = $"username/password is wrong.";
+                await new MessageDialog(msg).ShowAsync();
+            }
+            //clear User
+            user.username = string.Empty;
+            user.password = string.Empty;
+
+            //clear password
+            txtPassword.Password = string.Empty;
+            
+            //enable the button
+            this.btnLogin.IsEnabled = true;
+        }
     }
 }
